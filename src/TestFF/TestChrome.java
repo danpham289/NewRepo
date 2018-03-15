7cf3f9b74ffee2984144cbe3e3743fb09e6a24a8
danpham289 <37057544+danpham289@users.noreply.github.com>
2018-03-15 14:54:30 +0700

    add github

---
+++ src/TestFF/TestChrome.java
@@ -0,0 +1,15 @@
+package TestFF;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestChrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.gecko.driver","D:\\Selenium\\Drivers\\geckodriver-v0.19.1-win64\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://google.com");
+	}
+
+}
